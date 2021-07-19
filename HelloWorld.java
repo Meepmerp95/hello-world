d0f991982cb80e2872d3b515db25aa941e5529c4
Ric <ric_hcr95@hotmail.com>
2021-07-20 00:26:16 +0800

    Added Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    //creating main() method of the HelloWorld class    
+    public static void main(String args[]){
+        //Printing Hello World    
+        System.out.println("Hello, World!");
+    }
+}    
